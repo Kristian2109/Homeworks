ea37c590a206c395591d4b6ed2e4405ee44564ab
Kristian2109 <kristian.petrov1998@gmail.com>
2025-11-22 21:04:22 +0200

    Tic-tac-toe: convert to class based

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,70 +1,83 @@
-def min_max(board: list[list[int]], should_maximize: bool = True, depth: int = 0):
-    winner = check_winner(board, depth)
-    if winner != 0:
+from typing import TypedDict
+
+
+Board = list[list[int]]
+
+
+class MixMaxResult(TypedDict):
+    score: int
+    row: int
+    col: int
+
+
+class MinMaxAlgorithm:
+    def __init__(self, board: Board, is_x_bot: bool):
+        self.board: Board = board
+        self.is_x_bot: bool = is_x_bot
+
+    def get_best_move(self):
+        return self.min_max(should_maximize=self.is_x_bot)
+
+    def min_max(self, should_maximize: bool, depth: int = 0) -> MixMaxResult:
+        winner = self.check_winner(depth)
+        if winner != 0:
+            return {
+                "score": winner,
+                "row": -1,
+                "col": -1
+            }
+
+        if self.is_end():
+            return {
+                "score": winner,
+                "row": -1,
+                "col": -1
+            }
+
+        best_direction = [-1, -1]
+        best_direction_score = -20 if should_maximize else 20
+
+        for row in range(3):
+            for col in range(3):
+                if self.board[row][col] == 0:
+                    self.board[row][col] = 1 if should_maximize else -1
+                    result = self.min_max(should_maximize=not should_maximize, depth=depth + 1)
+                    self.board[row][col] = 0
+                    if self.is_first_better(result["score"], best_direction_score, should_maximize):
+                        best_direction = [row, col]
+                        best_direction_score = result["score"]
+
         return {
-            "score": winner,
-            "row": -1,
-            "col": -1
+            "row": best_direction[0],
+            "col": best_direction[1],
+            "score": best_direction_score
         }
 
-    if is_end(board):
-        return {
-            "score": winner,
-            "row": -1,
-            "col": -1
-        }
+    @staticmethod
+    def is_first_better(first: int, second: int, should_maximize: bool):
+        return first > second if should_maximize else first < second
 
-    best_direction = [-1, -1]
-    best_direction_score = -20 if should_maximize else 20
+    def check_winner(self, depth):
+        board = self.board
 
-    for row in range(3):
+        for row in range(3):
+            if board[row][0] != 0 and all(col == board[row][0] for col in board[row]):
+                return board[row][0] * 10 - depth * board[row][0]
+
         for col in range(3):
-            if board[row][col] == 0:
-                board[row][col] = 1 if should_maximize else -1
-                result = min_max(board, should_maximize=not should_maximize, depth=depth + 1)
-                board[row][col] = 0
-                if is_first_better(result["score"], best_direction_score, should_maximize):
-                    best_direction = [row, col]
-                    best_direction_score = result["score"]
+            if board[0][col] != 0 and all(board[row][col] == board[0][col] for row in range(3)):
+                return board[0][col] * 10 - depth * board[0][col]
 
-    return {
-        "row": best_direction[0],
-        "col": best_direction[1],
-        "score": best_direction_score
-    }
+        if board[0][0] != 0 and all(board[diag][diag] == board[0][0] for diag in range(0, 3)):
+            return board[0][0] * 10 - depth * board[0][0]
 
+        if board[2][2] != 0 and all(board[diag][2 - diag] == board[0][2] for diag in range(0, 3)):
+            return board[0][2] * 10 - - depth * board[0][2]
 
-def is_first_better(first: int, second: int, should_maximize):
-    if should_maximize:
-        return first > second
-    else:
-        return first < second
+        return 0
 
-
-def check_winner(board: list[list[int]], depth):
-    for row in range(3):
-        if board[row][0] != 0 and all(col == board[row][0] for col in board[row]):
-            return board[row][0] * 10 - depth * board[row][0]
-
-    for col in range(3):
-        if board[0][col] != 0 and all(board[row][col] == board[0][col] for row in range(3)):
-            return board[0][col] * 10 - depth * board[0][col]
-
-    if board[0][0] != 0 and all(board[diag][diag] == board[0][0] for diag in range(0, 3)):
-        return board[0][0] * 10 - depth * board[0][0]
-
-    if board[2][2] != 0 and all(board[diag][2 - diag] == board[0][2] for diag in range(0, 3)):
-        return board[0][2] * 10 - - depth * board[0][2]
-
-    return 0
-
-
-def is_end(board: list[list[int]]):
-    return all(all(col != 0 for col in row) for row in board)
-
-
-def solve_game(board: list[list[int]]):
-    min_max(board)
+    def is_end(self):
+        return all(all(col != 0 for col in row) for row in self.board)
 
 
 def read_board_framed():
@@ -102,9 +115,10 @@ def judge_mode():
 
     board = read_board_framed()
 
-    should_maximize = (player == "X")
+    is_x_bot = (player == "X")
 
-    result = min_max(board, should_maximize=should_maximize, depth=0)
+    min_max = MinMaxAlgorithm(board, is_x_bot)
+    result = min_max.get_best_move()
     print_move_from_minimax_result(result)
 
 
@@ -118,4 +132,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
